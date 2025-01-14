ba7803afd126732056378094cade5f8ca538b5de
MarceloFOCMenezes <marcelo.menezes@sptech.school>
2025-01-14 04:48:39 -0300

    Adicionando feature de tocar músicas do youtube, pausar e despausar músicas

--- index.js
+++ index.js
@@ -1,63 +1,90 @@
-const { Client, GatewayIntentBits, REST, Routes } = require('discord.js');
+const { Client, GatewayIntentBits, REST, Routes } = require("discord.js");
+const { Player, useMainPlayer, useQueue, useTimeline } = require("discord-player");
+const { YoutubeiExtractor } = require("discord-player-youtubei");
+const { SpotifyExtractor } = require("@discord-player/extractor");
+const { fetch, setGlobalDispatcher, Agent } = require("undici");
 
-const TOKEN = '';
-const CLIENT_ID = ''
-const GUILD_ID = ''
+setGlobalDispatcher(new Agent({ connect: { timeout: 60_000 } }));
+
+const TOKEN =
+  "";
+const CLIENT_ID = "";
+const GUILD_ID = "";
 
 // Lista de comandos
 const commands = [
-    {
-        name: 'somar',
-        description: 'Comando para somar dois números',
-        options: [{
-            type: 10,
-            name: 'n1',
-            description: 'Digite o primeiro número',
-            required: true
-        },
-        {
-            type: 10,
-            name: 'n2',
-            description: 'Digite o segundo número',
-            required: true
-        }
-        ],
-    },
-    {
-        name: 'multiplicar',
-        description: 'Comando para somar multiplicar dois números',
-        options: [{
-            type: 10,
-            name: 'n1',
-            description: 'Digite o primeiro número',
-            required: true
-        },
-        {
-            type: 10,
-            name: 'n2',
-            description: 'Digite o segundo número',
-            required: true
-        }
-        ],
-    },
-    {
-        name: 'slack',
-        description: 'Olá Slack!'
-    },
-    {
-        name: 'dog',
-        description: 'Cachorro aleatório estilo quitéria...'
-    },
-    {
-        name: 'adivinhar',
-        description: 'tente adivinhar um número de 1 a 10 que o bot escolheu',
-        options: [{
-            type: 4,
-            name: 'num',
-            description: 'Digite o seu chute',
-            required: true
-        }]
-    }
+  {
+    name: "somar",
+    description: "Comando para somar dois números",
+    options: [
+      {
+        type: 10,
+        name: "n1",
+        description: "Digite o primeiro número",
+        required: true,
+      },
+      {
+        type: 10,
+        name: "n2",
+        description: "Digite o segundo número",
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "multiplicar",
+    description: "Comando para somar multiplicar dois números",
+    options: [
+      {
+        type: 10,
+        name: "n1",
+        description: "Digite o primeiro número",
+        required: true,
+      },
+      {
+        type: 10,
+        name: "n2",
+        description: "Digite o segundo número",
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "slack",
+    description: "Olá Slack!",
+  },
+  {
+    name: "dog",
+    description: "Cachorro aleatório estilo quitéria...",
+  },
+  {
+    name: "adivinhar",
+    description: "tente adivinhar um número de 1 a 10 que o bot escolheu",
+    options: [
+      {
+        type: 4,
+        name: "num",
+        description: "Digite o seu chute",
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "tocar",
+    description: "Tocar uma música no canal de voz",
+    options: [
+      {
+        type: 3, // Tipo 3 corresponde a STRING
+        name: "música",
+        description: "Digite o nome ou link da música",
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "pausar",
+    description:"Pausar ou despausar a música atual"
+  }
 ];
 
 //Enviar comandos para o discord
@@ -80,21 +107,41 @@ const rest = new REST({ version: "10" }).setToken(TOKEN);
 
 //Criando conexão para o bot interprestar as mensagens
 const client = new Client({
+  restTimeOffset: 0,
+  shards: "auto",
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildVoiceStates,
   ],
+  retryLimit: 3,
+  timeout: 20000,
 });
+
+const player = new Player(client, {
+  leaveOnEnd: true,
+  leaveOnStop: true,
+  leaveOnEmpty: true,
+  leaveOnEmptyCooldown: 5000,
+  altoSelfDeaf: true,
+  initialVolume: 50,
+  bufferingTimeout: 10000,
+  skipFFmpeg: false,
+  connectionTimeout: 10000,
+});
+
+player.extractors.register(SpotifyExtractor, {});
+player.extractors.register(YoutubeiExtractor, {});
 
 client.once("ready", () => {
   console.log(`Bot está online como ${client.user.tag}!`);
+  client.user.setActivity("Your Soung", { type: "LISTENING" });
 });
 
 client.on("messageCreate", (message) => {
   // Verifica se a mensagem não é do próprio bot
   if (message.author.bot) return;
-
   // Envia uma mensagem ao detectar "oi"
   if (message.content.toLowerCase() === "oi") {
     message.channel.send(`olá ${message.author.username}!`);
@@ -106,6 +153,72 @@ client.on("messageCreate", (message) => {
 client.on("interactionCreate", async (interaction) => {
   // verificando se a interação é a partir de um comando
   if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === "tocar") {
+    const canal = interaction.member.voice.channel;
+    if (!canal)
+      return interaction.reply(
+        "Você precisa entrar em um canal de voz, Quica!"
+      );
+
+    const musica = interaction.options.getString("música");
+    if (!musica)
+      return interaction.reply(
+        "Digite o nome ou um link de uma música, Quica!"
+      );
+
+    const queue = useMainPlayer().nodes.create(interaction.guild.id, {
+      metadata: {
+        channel: interaction.channel,
+      },
+      selfDeaf: true,
+    });
+
+    try {
+      // Verifica se o bot já está conectado
+      if (!queue.connection) {
+        await queue.connect(canal);
+        console.log("Bot conectado ao canal de voz");
+      }
+
+      // Procura pela música
+      const track = await player.search(musica, {
+        requestedBy: interaction.user,
+      });
+      console.log(track);
+      if (!track || !track.tracks.length) {
+        return interaction.reply("Nenhuma música encontrada, irmã Quica!");
+      }
+
+      // Adiciona a faixa à fila de reprodução
+      queue.addTrack(track.tracks[0]);
+
+      // Verifica se a música não está tocando e começa a reprodução
+      if (!queue.isPlaying()) {
+        await queue.node.play();
+        console.log("Música tocando agora...");
+      }
+
+      interaction.reply(`Tocando agora: ${track.tracks[0].title}`);
+    } catch (error) {
+      console.error("Erro ao tentar conectar ou tocar a música:", error);
+      queue.delete();
+      return await interaction.reply("Não foi possível se conectar ao canal.");
+    }
+  }
+  if(interaction.commandName == "pausar"){
+    const timeline = useTimeline({
+      node: interaction.guild
+    })
+    if (!timeline) {
+      return interaction.reply('Este servidor não tem uma sessão de player ativa, Culega!');
+    }
+
+
+    const wasPaused = timeline.paused;
+    wasPaused ? timeline.resume() : timeline.pause();
+    return interaction.reply(`A música está: ${wasPaused ? 'Tocando' : 'Pausada'}.`);
+  }
 
   if (interaction.commandName === "somar") {
     var n1 = interaction.options.getNumber("n1");
@@ -119,54 +232,50 @@ client.on("interactionCreate", async (interaction) => {
     var n2 = interaction.options.getNumber("n2");
     var resultado = n1 * n2;
 
-        console.log(`números que o usuario digitou foram ${n1} e ${n2}`)
-        interaction.reply(`${resultado}`)
-    }
-    else if (interaction.commandName === 'multiplicar') {
-        var n1 = interaction.options.getNumber('n1')
-        var n2 = interaction.options.getNumber('n2')
-        var resultado = n1 * n2
+    console.log(`números que o usuario digitou foram ${n1} e ${n2}`);
+    interaction.reply(`${resultado}`);
+  } else if (interaction.commandName === "multiplicar") {
+    var n1 = interaction.options.getNumber("n1");
+    var n2 = interaction.options.getNumber("n2");
+    var resultado = n1 * n2;
 
-        console.log(`números que o usuario digitou foram ${n1} e ${n2}`)
-        interaction.reply(`${resultado}`)
-    }
-    else if (interaction.commandName === 'slack') {
-        interaction.reply('Olá Slack')
-    } else if (interaction.commandName === 'dog') {
-        const { gifUrl, texto } = gerarGif()
-        const embeds = {
-            color: 0x00ff00,
-            title: texto,
-            image: { url: gifUrl }
-        }
-        interaction.reply({ embeds: [embeds] });
-    }
-    else if (interaction.commandName === 'adivinhar') {
-        var numeroUsuario = interaction.options.getInteger('num')
-        var randomNumber = Math.floor(Math.random() * 10) + 1
+    console.log(`números que o usuario digitou foram ${n1} e ${n2}`);
+    interaction.reply(`${resultado}`);
+  } else if (interaction.commandName === "slack") {
+    interaction.reply("Olá Slack");
+  } else if (interaction.commandName === "dog") {
+    const { gifUrl, texto } = gerarGif();
+    const embeds = {
+      color: 0x00ff00,
+      title: texto,
+      image: { url: gifUrl },
+    };
+    interaction.reply({ embeds: [embeds] });
+  } else if (interaction.commandName === "adivinhar") {
+    var numeroUsuario = interaction.options.getInteger("num");
+    var randomNumber = Math.floor(Math.random() * 10) + 1;
 
-        if (numeroUsuario != randomNumber) {
-            console.log('numero aleatorio', randomNumber)
-            const gif = gerarGif()
-            const embeds = {
-                color: 0x00ff00,
-                title: 'Você não acertou porquinteria!!',
-                image: { url: gif }
-            }
-            interaction.reply({ embeds: [embeds] })
-        }
-        else {
-            console.log('numero aleatorio', randomNumber)
-            const gif = gerarGif()
-            const embeds = {
-                color: parseInt('A18262',16),
-                title: 'Você acertou a porquinteria!!',
-                image: { url: gif }
-            }
-            interaction.reply({ embeds: [embeds] })
-        }
+    if (numeroUsuario != randomNumber) {
+      console.log("numero aleatorio", randomNumber);
+      const gif = gerarGif();
+      const embeds = {
+        color: 0x00ff00,
+        title: "Você não acertou porquinteria!!",
+        image: { url: gif },
+      };
+      interaction.reply({ embeds: [embeds] });
+    } else {
+      console.log("numero aleatorio", randomNumber);
+      const gif = gerarGif();
+      const embeds = {
+        color: parseInt("A18262", 16),
+        title: "Você acertou a porquinteria!!",
+        image: { url: gif },
+      };
+      interaction.reply({ embeds: [embeds] });
     }
-})
+  }
+});
 // Lista de Gifs
 const gif = [
   "https://media.tenor.com/LmXWDEt07L0AAAAM/cachorro-batendo-panela-n-peita.gif",
@@ -185,7 +294,7 @@ const gif = [
 ];
 
 function gerarGif() {
-  const indexGif = Math.floor(Math.random() * (gif.length - 1));
+  const indexGif = Math.floor(Math.random() * gif.length);
   console.log(indexGif);
   var texto = "";
   switch (indexGif) {
@@ -232,6 +341,8 @@ function gerarGif() {
   const gifUrl = gif[indexGif];
   return { gifUrl, texto };
 }
+
+module.exports = { player, client };
 
 // Login do bot
 client.login(TOKEN);
